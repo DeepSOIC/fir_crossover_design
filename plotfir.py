4ded10d70e03552f258103f32598ef57905cac7c
DeepSOIC <vv.titov@gmail.com>
2022-05-09 20:48:53 +0300

    merge plots, new figure management

--- plotfir.py
+++ plotfir.py
@@ -1,9 +1,58 @@
-import myWindowFuncs
+figures = None
+ax_wf = None
+ax_sp = None
 
-def plot_fir(n_pts = 731, f_x = 250.0, window = ('tukey', 0.5), fs = 44100, pass_zero = 'lowpass', label = 'main'):
+
+def fig(split = False):
+    """fig(split = False): creates new figures to plot onto"""
+    global figures, ax_wf, ax_sp
+
+    import matplotlib.pyplot as plt
+    plt.ion()
+    
+    if split:
+        figure1 = plt.figure()
+        ax_wf = figure1.add_subplot(
+            1,1,1
+        )
+        figure2 = plt.figure()
+        ax_sp = figure2.add_subplot(
+            1,1,1,
+            xscale='log', xlim=(20.0, 20e3),
+            yscale='log', ylim=(1e-3, 3)
+        )   
+        figures = [figure1, figure2]
+    else:
+        figure = plt.figure(figsize = (15, 5))
+        ax_wf = figure.add_subplot(
+            1,3,1
+        )
+        ax_sp = figure.add_subplot(
+            1,3,(2,3),
+            xscale='log', xlim=(20.0, 20e3),
+            yscale='log', ylim=(1e-3, 3)
+        )
+        figures = [figure,]
+    
+    ax_wf.set_title("waveforms")
+    ax_wf.set_ylabel("amplitude")
+    ax_wf.set_xlabel("time [samples]")
+    ax_sp.set_xlabel("frequency [Hz]")
+    ax_sp.set_ylabel("amplitude")
+    ax_sp.grid(True, which= 'major', color='#00ff00ff', linewidth=0.6)
+    ax_sp.grid(True, which= 'minor', color='#ffff00ff', linewidth=0.6)
+    
+    for fig in figures:
+        fig.canvas.mpl_connect('close_event', on_close)
+
+def plot_firwin(n_pts = 731, f_x = 250.0, window = ('tukey', 0.5), fs = 44100, pass_zero = 'lowpass', label = 'main'):
+    global figures, ax_wf, ax_sp
+
     import scipy.signal
     import numpy as np
+    import myWindowFuncs
     
+
     try:
         win = scipy.signal.windows.get_window(window, n_pts)
     except Exception: raise()
@@ -30,25 +79,21 @@ def plot_fir(n_pts = 731, f_x = 250.0, window = ('tukey', 0.5), fs = 44100, pass
             pass_zero= pass_zero
         )
     
-    import matplotlib.pyplot as plt
+
+
+    if figures is None:
+        fig()
     
-    plt.ion()
-    
-    plt.figure('waveform'+label if label else None)
-    plt.plot(
+    ax_wf.plot(
         fir/np.abs(fir).max(), 
         label= "wf ({window} window)".format(window= str(window))
     )
-    plt.plot(
-        win,
+    ax_wf.plot(
+        win/np.abs(win).max(),
         label= "{window} window".format(window= str(window))
     )
-    plt.title("waveforms")
-    plt.ylabel("amplitude")
-    plt.xlabel("time [samples]")
-    plt.legend()
     
-    plt.show()
+    ax_wf.legend()    
     
     inversefir = fir.copy()
     inversefir[int((n_pts-1)/2)] -= 1.0
@@ -57,23 +102,24 @@ def plot_fir(n_pts = 731, f_x = 250.0, window = ('tukey', 0.5), fs = 44100, pass
     frequencies, spec = AST.audio_fft(AST.pad(fir, 65536), normalize= 'filter')
     frequencies, invspec = AST.audio_fft(AST.pad(inversefir, 65536), normalize= 'filter')
     
-    plt.figure('spectrum'+label if label else None)
-    plt.gca(
-        label='spectral',
-        xscale='log', xlim=(20.0, 20e3),
-        yscale='log', ylim=(1e-3, 3)
-    )
-    plt.xlabel("frequency [Hz]")
-    plt.ylabel("amplitude")
     #import uuid
     #id = uuid.uuid4()
-    plt.plot(
+    ax_sp.plot(
         np.append(frequencies, frequencies[::-1]), 
         np.append(spec, invspec[::-1]), 
         label= str(window)
     )
-    plt.axvline(250.0, color='#00ff00ff')
-    plt.grid(True, which= 'major', color='#00ff00ff', linewidth=0.6)
-    plt.grid(True, which= 'minor', color='#ffff00ff', linewidth=0.6)
-    plt.legend()
-    plt.show()
+    ax_sp.axvline(250.0, color='#00ff00ff')
+    ax_sp.legend()
+
+    import matplotlib.pyplot as plt
+    tight_layout()
+
+
+def on_close(event):
+    global figures
+    figures = None
+
+def tight_layout():
+    for fig in figures:
+        fig.tight_layout()
