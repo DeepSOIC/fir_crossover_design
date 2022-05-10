3e6d049052a08a4a9fe82e73a8f16bd241051ac0
DeepSOIC <vv.titov@gmail.com>
2022-05-10 15:36:30 +0300

    add remez method

--- plotfir.py
+++ plotfir.py
@@ -51,6 +51,18 @@ def fig(split = False, label = None):
         conns.append((conn, fig))
         
 
+def plot_remez(n_pts = 731, f_x = 250.0, t_width = 30):
+    import scipy.signal
+    fir = scipy.signal.remez(
+        731, 
+        [0, f_x - t_width/2, 
+        f_x + t_width/2, fs/2],
+        [1.0,0.0],
+        fs=fs
+    )
+    
+    plot_fir(fir, None, 'remez {t_width}'.format(t_width = t_width))
+
 def plot_firwin(n_pts = 731, f_x = 250.0, window = ('tukey', 0.5), pass_zero = 'lowpass', label = None):
     global figures, ax_wf, ax_sp
 
@@ -84,25 +96,30 @@ def plot_firwin(n_pts = 731, f_x = 250.0, window = ('tukey', 0.5), pass_zero = '
             fs= fs, # sample rate
             pass_zero= pass_zero
         )
+    label = "{window} window".format(window= str(window)) if label is None else label
+    plot_fir(fir, win, label)
     
 
+def plot_fir(fir, win, label):
+    import numpy as np
 
     if figures is None:
-        fig(label= str(window) if label is None else label)
+        fig(label= label)
     
     ax_wf.plot(
         fir/np.abs(fir).max(), 
-        label= "wf ({window} window)".format(window= str(window))
+        label= "wf ({label})".format(label= label)
     )
-    ax_wf.plot(
-        win/np.abs(win).max(),
-        label= "{window} window".format(window= str(window))
-    )
+    if win is not None:
+        ax_wf.plot(
+            win/np.abs(win).max(),
+            label= label
+        )
     
     ax_wf.legend()    
     
     inversefir = fir.copy()
-    inversefir[int((n_pts-1)/2)] -= 1.0
+    inversefir[int((len(fir)-1)/2)] -= 1.0
     
     import audioSpectralTools as AST
     frequencies, spec = AST.audio_fft(AST.pad(fir, 65536), normalize= 'filter', fs= fs)
@@ -113,14 +130,16 @@ def plot_firwin(n_pts = 731, f_x = 250.0, window = ('tukey', 0.5), pass_zero = '
     ax_sp.plot(
         np.append(frequencies, frequencies[::-1]), 
         np.append(spec, invspec[::-1]), 
-        label= str(window)
+        label= label
     )
     ax_sp.axvline(250.0, color='#00ff00ff')
     ax_sp.legend()
 
     import matplotlib.pyplot as plt
     tight_layout()
-    
+
+
+
 def save():
     figures[0].savefig(r'S:\tmp\{ft}.png'.format(ft=figures[0].get_label()))
     figures[0].clear()
